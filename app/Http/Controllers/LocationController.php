34b9fc4431801681b25885da32cbe217ac5e905c
simonojok19 <simonojok19@gmail.com>
2020-05-22 22:29:12 +0300

    added lone index actions to all controllers

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class LocationController extends Controller
 {
-    //
+    public function index() {
+        return view('events.index');
+    }
 }
--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class MapsController extends Controller
 {
-    //
+    public function index() {
+        return view('events.index');
+    }
 }
