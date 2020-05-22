974aae35ce31d8aba42ca4b40c04129dcbd19ea3
simonojok19 <simonojok19@gmail.com>
2020-05-22 20:35:54 +0300

    added my first template

--- resources/views/index/welcome.blade.php
+++ resources/views/index/welcome.blade.php
@@ -1,11 +1,8 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Welcome to HackerPair</title>
-    </head>
-    <body>
-        <h1>Welcome to HackerPair</h1>
-        <a href="{{ route('events.show', ['id' => 42]) }}">Laravel Hacking and Coffee</a>
-    </body>
-</html>
+@extends('layout.app')
+@section('content')
+    <h1>HackerPair Helps You Build Stuff Faster.</h1>
+    <p>
+        Learn, teach, hack, and make friends with developers
+        in your city
+    </p>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome to HackerPair</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
