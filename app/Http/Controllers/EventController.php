ccb7190b5936abe56d2ae45cff213a7980b05d4f
simonojok19 <simonojok19@gmail.com>
2020-05-22 18:23:31 +0300

    added optional value on routes

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function show($id) {
+        dd($id);
+    }
+
+    public function category($category, $subcategory = 'all') {
+        dd("Category: {$category} Subcategory: {$subcategory}");
+    }
 }
