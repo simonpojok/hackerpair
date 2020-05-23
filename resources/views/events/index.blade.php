eefca6357f3a03022bfb490f18d03f689d357430
simonojok19 <simonojok19@gmail.com>
2020-05-23 23:51:53 +0300

    error fixed

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,4 +1,11 @@
 @extends('layout.app')
 @section('content')
     <h1>Events</h1>
+    <ul>
+        @forelse($events as $event)
+            <li>{{ $event -> name }}</li>
+        @empty
+            <li>No events</li>
+        @endforelse
+    </ul>
 @endsection
