15c45ab079b48d561aaf2008f7a9b716e7d41325
simonojok19 <simonojok19@gmail.com>
2020-05-23 23:03:02 +0300

    created seeder with faker

--- database/seeds/EventTableSeeder.php
+++ database/seeds/EventTableSeeder.php
@@ -9,12 +9,16 @@ class EventTableSeeder extends Seeder
     public function run()
     {
         DB::table('events') -> truncate();
-        Event::create([
-            'name' => "Laravel and Coffee",
-            'city' => "Dublin",
-            'venue' => "Cup of Joe",
-            "description" => "Let's learn Laravel together"
-        ]);
+        $faker = \Faker\Factory::create();
+
+        foreach (range(1, 50) as $index) {
+            Event::create([
+                'name' => $faker -> sentence(2),
+                'city' => $faker -> city,
+                'venue' => $faker -> company,
+                "description" => $faker -> paragraph(1, true)
+            ]);
+        }
 
     }
 }
