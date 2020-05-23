43e4f9ef34e0569424e6b56a93705a13aaf5db93
simonojok19 <simonojok19@gmail.com>
2020-05-23 23:29:31 +0300

    added the first query

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
 use Illuminate\Http\Request;
 
 class EventsController extends Controller
@@ -13,7 +14,8 @@ class EventsController extends Controller
      */
     public function index()
     {
-        //
+        $events = Event::all();
+        return view('events.index') -> with('events', $events);
     }
 
     /**
