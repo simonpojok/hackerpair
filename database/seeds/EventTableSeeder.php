d9f885348fe3b1ad436bb37ba03c557c2f15596d
simonojok19 <simonojok19@gmail.com>
2020-05-23 22:47:47 +0300

    added seeders

--- database/seeds/EventTableSeeder.php
+++ database/seeds/EventTableSeeder.php
@@ -6,6 +6,16 @@ class EventTableSeeder extends Seeder
 {
     public function run()
     {
-        //
+        DB::table('events') -> truncate();
+
+        for ($index = 0; $index <= 20; $index++) {
+            Event::create([
+                'name' => "Laravel and Coffee",
+                'city' => "Dublin",
+                'venue' => "Cup of Joe",
+                "description" => "Let's learn Laravel together"
+            ]);
+        }
+
     }
 }
